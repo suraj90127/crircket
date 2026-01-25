7fcf52229ddd0938b93c5afc39ea0bd63cf0a9d9
Suraj90127 <surajsingh26574@gmail.com>
2026-01-25 22:40:29 +0530

    second commit

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
-    // baseURL: "/api",
+    // baseURL: "http://localhost:5000/api",
+    baseURL: "https://aura444.org/api",
 });
 
 export default api;
 
 
-export const host = "ws://localhost:5000";
-// export const host = "/";
+// export const host = "ws://localhost:5000";
+export const host = "https://aura444.org/";
