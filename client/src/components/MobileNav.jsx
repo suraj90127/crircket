7c28ea0d359e4670ce78133e9d4df2c892ba0f51
Anoop4525 <128415443+Anoop4525@users.noreply.github.com>
2026-01-30 00:38:09 -0800

    from Anoop

--- client/src/components/MobileNav.jsx
+++ client/src/components/MobileNav.jsx
@@ -11,8 +11,8 @@ export default function MobileNavigation() {
     { name: "Profile", icon: <FaUserCircle className="h-5 w-5" />, href: "/profile" },
     { name: "In-Play", icon: <FaPlayCircle className="h-5 w-5" />, href: "/in-play" },
     { name: "Home", icon: <IoHome className="h-5 w-5" />, href: "/" },
-    { name: "Deposit", icon: <FaPiggyBank className="h-5 w-5" />, href: "/deposit" },
-    { name: "Withdrawal", icon: <FaWallet className="h-5 w-5" />, href: "/withdrawal" },
+    { name: "Deposit", icon: <FaPiggyBank className="h-5 w-5" />, href: "/depositwithdrawal" },
+    { name: "Withdrawal", icon: <FaWallet className="h-5 w-5" />, href: "/depositwithdrawal" },
   ];
 
   return (
