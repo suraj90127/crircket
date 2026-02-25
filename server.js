a9d9c928aa3ee01cc44d34076bd3f09e4387e9a0
Suraj90127 <surajsingh26574@gmail.com>
2026-02-25 14:24:18 +0530

    done

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization","x-domain"],
   })
 );
 
