4e0f5adf78012e71c8ade1b03117d1f49e758734
Suraj90127 <surajsingh26574@gmail.com>
2026-02-25 14:27:41 +0530

    change cros

--- server.js
+++ server.js
@@ -38,13 +38,7 @@ const server = http.createServer(app);
 // Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:5175",
-      "https://aura444.org",
-      "https://www.reddy111.bet"
-    ],
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization","x-domain"],
